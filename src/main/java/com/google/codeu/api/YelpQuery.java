7a4be68a5443a9d6af7427244b35a0c6c5b556ef
aokennard <aokennard@gmail.com>
2019-03-25 02:24:23 -0500

    Need to handle exceptions, include api header

--- src/main/java/com/google/codeu/api/YelpQuery.java
+++ src/main/java/com/google/codeu/api/YelpQuery.java
@@ -1,12 +1,17 @@
+import java.util.Map;
 import java.util.HashMap;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URI;
-import org.apache.http.client.utils.URIBuilder
+
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.commons.io.IOUtils;
 
 import com.google.gson.Gson;
 
@@ -22,23 +27,31 @@ public class YelpQuery {
     builder.setScheme("https").setHost("api.yelp.com").setPath("/v3/businesses/search");
     // Adding parameters to search string: TODO error checking. note, we need a location here in the parameters at least.
     for(Map.Entry<String, String> entry : parameters.entrySet()) {
-      String key = entry.getKey();
-      String value = entry.getValue();
-      builder.setParameter(key, value);
+      builder.setParameter(entry.getKey(), entry.getValue());
     }
     URI uri = builder.build();
     return uri.toURL();
   }
 
-  public Gson getQueryResponse(URL url) {
+  public String getQueryResponse(URL url) {
     URLConnection connection = url.openConnection();
     InputStream stream = connection.getInputStream();
-    return new Gson(); // get data from stream;
+    String result = IOUtils.toString(stream, StandardCharsets.UTF_8);
+    return new Gson().toJson(result); // get data from stream;
   }
 
-  static string readFile(String path) {
-    BufferedReader reader = new BufferedReader(new FileReader(file));
+  public static String readFile(String path) {
+    BufferedReader reader = new BufferedReader(new FileReader(path));
     return reader.readLine(); // TODO: error handling
+  }
+  
+  public static void main(String[] args) {
+    YelpQuery query = new YelpQuery();
+    Map<String, String> params = new HashMap<String, String>();
+    params.put("location", "Chicago");
+    params.put("term", "ice cream");
+    URL url = query.createQuery(params);
+    System.out.println(query.getQueryResponse(url));
   }
 
 }
