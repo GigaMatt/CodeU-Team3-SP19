a3d8d7581fee006b5bad4dd0b45c37d9a5788c83
aokennard <aokennard@gmail.com>
2019-03-25 02:30:25 -0500

    Did bad exception handling, more imports, added dependencies.

--- src/main/java/com/google/codeu/api/YelpQuery.java
+++ src/main/java/com/google/codeu/api/YelpQuery.java
@@ -1,6 +1,8 @@
 import java.util.Map;
 import java.util.HashMap;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.InputStream;
@@ -9,6 +11,8 @@ import java.nio.charset.StandardCharsets;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.MalformedURLException;
 
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.commons.io.IOUtils;
@@ -18,11 +22,11 @@ import com.google.gson.Gson;
 public class YelpQuery {
   private String apiKey;
   
-  public YelpQuery() {
+  public YelpQuery() throws IOException {
     this.apiKey = readFile("api.key");
   }
   // just uses basic business search; https://www.yelp.com/developers/documentation/v3/business_search
-  public URL createQuery(Map<String, String> parameters) {
+  public URL createQuery(Map<String, String> parameters) throws URISyntaxException, MalformedURLException, IOException {
     URIBuilder builder = new URIBuilder();
     builder.setScheme("https").setHost("api.yelp.com").setPath("/v3/businesses/search");
     // Adding parameters to search string: TODO error checking. note, we need a location here in the parameters at least.
@@ -33,19 +37,19 @@ public class YelpQuery {
     return uri.toURL();
   }
 
-  public String getQueryResponse(URL url) {
+  public String getQueryResponse(URL url) throws IOException, FileNotFoundException {
     URLConnection connection = url.openConnection();
     InputStream stream = connection.getInputStream();
     String result = IOUtils.toString(stream, StandardCharsets.UTF_8);
     return new Gson().toJson(result); // get data from stream;
   }
 
-  public static String readFile(String path) {
+  public static String readFile(String path) throws IOException, FileNotFoundException {
     BufferedReader reader = new BufferedReader(new FileReader(path));
     return reader.readLine(); // TODO: error handling
   }
   
-  public static void main(String[] args) {
+  public static void main(String[] args) throws URISyntaxException, IOException {
     YelpQuery query = new YelpQuery();
     Map<String, String> params = new HashMap<String, String>();
     params.put("location", "Chicago");
